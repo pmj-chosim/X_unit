cbe37189cb788a7b9c17465e3dc83724d8dff8ad
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2024-07-25 11:35:59 +0000

    수정본

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+
+            if (candidate % 2 == 0)
+            {
+                return false;
+            }
+            
+            for (int i = 3; i<= Math.Sqrt(candidate); i++)
+                if (candidate % i == 0) return false;
+
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
